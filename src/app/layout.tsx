fce4cf99f43e3bf69c6e0a8c1fc94b4d221453cf
Sentaro <ui-10@outlook.jp>
2023-11-27 17:39:30 +0900

    css変更に伴うimportの変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 // import { Inter } from 'next/font/google'
 import Favicon from '../../public/images/favicon/solo.ico';
-import './globals.css';
+import '../css/globals.css';
 
 export const metadata: Metadata = {
   title: {
