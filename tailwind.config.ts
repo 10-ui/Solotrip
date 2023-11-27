61abeb9ce4c7bc1283fce95146ca2a0043cefc17
Sentaro <ui-10@outlook.jp>
2023-11-27 17:39:51 +0900

    プロパティの追加。独自のものを採用

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,22 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    borderRadius: {
+      2: '2px',
+      42: '42px',
+    },
     extend: {
       colors: {
-        intro: '#009B9F',
+        main: '#009B9F',
+        accent: '#E07510',
+        bases: '#EEF2E8',
+        form: '#2222224D',
+      },
+      spacing: {
+        40: '40px',
+        42: '42px',
+        160: '160px',
+        350: '350px',
       },
     },
   },
