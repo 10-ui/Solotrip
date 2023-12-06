428411f00d56af1ee48b9abd5fab66aadd360f17
Sentaro <ui-10@outlook.jp>
2023-12-06 19:48:16 +0900

    Fix import and update form styles

--- src/app/passwordreset/page.tsx
+++ src/app/passwordreset/page.tsx
@@ -1,6 +1,7 @@
+'use client';
 import { supabase } from '../../../utils/supabase';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function PasswordReset() {
   const router = useRouter();
@@ -25,32 +26,54 @@ export default function PasswordReset() {
 
   return (
     <>
-      <main>
-        <div>
+      <main className="w-350 mx-auto mt-100 mb-160 text-bases">
+        <h1 className="text-4xl font-bold text-left mb-8">リセットフォーム</h1>
           <form onSubmit={onSubmit}>
             <div>
-              <label>パスワード</label>
+              <label
+                className="w-full text-base mb-3 font-bold"
+                htmlFor="password"
+              >
+                パスワード
+              </label>
               <input
                 type="password"
+                name="password"
+                className="w-full rounded-2 py-3 px-4 mb-4 text-base text-darks"
+                id="password"
+                placeholder="パスワード(5文字以上)"
                 required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div>
-              <label>パスワード（確認）</label>
+            <div className="mb-8">
+              <label
+                className="w-full text-base mb-3 font-bold"
+                htmlFor="passwordConf"
+              >
+                パスワード(確認)
+              </label>
               <input
                 type="password"
+                name="passwordConf"
+                className="w-full rounded-2 py-3 px-4 mb-4 text-base text-darks"
+                id="passwordConf"
+                placeholder="パスワード(確認)"
                 required
                 value={passwordConf}
                 onChange={(e) => setPasswordConf(e.target.value)}
               />
             </div>
             <div>
-              <button type="submit">パスワード変更</button>
+              <button
+                className="bg-accent w-full font-bold text-base py-3 px-4 rounded-42"
+                type="submit"
+              >
+                パスワード変更
+              </button>
             </div>
           </form>
-        </div>
       </main>
     </>
   );
